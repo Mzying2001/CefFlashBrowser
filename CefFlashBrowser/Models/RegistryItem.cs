f568bee109321f020b8543adb2d8f9d1cb0f6dea
Mzying2001 <Mzying2001@qq.com>
2021-03-09 01:05:47 +0800

    Add RegistryItem class

---
+++ CefFlashBrowser/Models/RegistryItem.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefFlashBrowser.Models
+{
+    class RegistryItem : IDisposable, IEnumerable<KeyValuePair<string, object>>
+    {
+        private RegistryKey _registryKey;
+
+        public RegistryItem(RegistryKey parent, string itemName)
+        {
+            if (!parent.GetSubKeyNames().Contains(itemName))
+                parent.CreateSubKey(itemName);
+            _registryKey = parent.OpenSubKey(itemName, true);
+        }
+
+        public void Dispose()
+        {
+            _registryKey.Close();
+            _registryKey.Dispose();
+        }
+
+        public object this[string name]
+        {
+            get => _registryKey.GetValue(name);
+            set
+            {
+                if (value == null)
+                    _registryKey.DeleteValue(name);
+                else
+                    _registryKey.SetValue(name, value);
+            }
+        }
+
+        public T GetValue<T>(string name)
+        {
+            var value = this[name];
+            return value == null ? default : (T)Convert.ChangeType(value, typeof(T));
+        }
+
+        public RegistryItem SubKey(string name)
+        {
+            return new RegistryItem(_registryKey, name);
+        }
+
+        public IEnumerator<KeyValuePair<string, object>> GetEnumerator()
+        {
+            foreach (string name in _registryKey.GetValueNames())
+                yield return new KeyValuePair<string, object>(name, this[name]);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
--- CefFlashBrowser/Models/Settings.cs
+++ CefFlashBrowser/Models/Settings.cs
@@ -9,66 +9,29 @@ namespace CefFlashBrowser.Models
 {
     static class Settings
     {
-        #region Registry
-
-        private static RegistryKey _currentUser, _regKey;
+        private static RegistryItem _reg;
 
         public static void Init()
         {
-            _currentUser = Registry.CurrentUser;
-            string keyName = @"SOFTWARE\CefFlashBrowser";
-
-            if (!_currentUser.ContainsKey(keyName))
-                _currentUser.CreateSubKey(keyName);
-
-            _regKey = _currentUser.OpenSubKey(keyName, true);
+            _reg = new RegistryItem(Registry.CurrentUser, @"SOFTWARE\CefFlashBrowser");
         }
-
-        public static void Close()
-        {
-            _currentUser.Close();
-            _regKey.Close();
-        }
-
-        private static bool ContainsKey(this RegistryKey registryKey, string keyName)
-        {
-            return registryKey.GetSubKeyNames().Contains(keyName);
-        }
-
-        public static void WriteValue(string key, object value)
-        {
-            _regKey.SetValue(key, value.ToString());
-        }
-
-        public static string ReadValue(string key)
-        {
-            return _regKey.GetValue(key)?.ToString();
-        }
-
-        public static T ReadValue<T>(string key)
-        {
-            var value = ReadValue(key);
-            return value == null ? default : (T)Convert.ChangeType(value, typeof(T));
-        }
-
-        #endregion
 
         public static string Language
         {
-            get => ReadValue("Language") ?? "en-US";
-            set => WriteValue("Language", value);
+            get => _reg.GetValue<string>("Language") ?? "en-US";
+            set => _reg["Language"] = value;
         }
 
         public static SearchEngine.Engine SearchEngine
         {
-            get => (SearchEngine.Engine)ReadValue<int>("SearchEngine");
-            set => WriteValue("SearchEngine", (int)value);
+            get => (SearchEngine.Engine)_reg.GetValue<int>("SearchEngine");
+            set => _reg["SearchEngine"] = (int)value;
         }
 
         public static int AddressBarFunction
         {
-            get => ReadValue<int>("AddressBarFunction");
-            set => WriteValue("AddressBarFunction", value);
+            get => _reg.GetValue<int>("AddressBarFunction");
+            set => _reg["AddressBarFunction"] = value;
         }
     }
 }
