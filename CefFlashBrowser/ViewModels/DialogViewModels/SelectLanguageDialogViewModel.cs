8a201dfc958705ee878fc9aac8b47dec4acb7731
Mzying2001 <Mzying2001@qq.com>
2021-04-03 21:37:40 +0800

    Add SelectLanguageDialog

---
+++ CefFlashBrowser/ViewModels/DialogViewModels/SelectLanguageDialogViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CefFlashBrowser.Commands;
+using CefFlashBrowser.Models.StaticData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefFlashBrowser.ViewModels.DialogViewModels
+{
+    class SelectLanguageDialogViewModel : NotificationObject
+    {
+        public DelegateCommand SelectLanguageCommand { get; set; }
+
+        private void SelectLanguage(string language)
+        {
+            LanguageManager.CurrentLanguage = language;
+        }
+
+        public SelectLanguageDialogViewModel()
+        {
+            SelectLanguageCommand = new DelegateCommand(p => SelectLanguage(p?.ToString()));
+        }
+    }
+}
