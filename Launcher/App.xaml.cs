40a0d7f138fafd0de28786f60cf95050091cb801
Mzying2001 <Mzying2001@qq.com>
2021-04-07 23:56:29 +0800

    Add Launcher

---
+++ Launcher/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Launcher.Scripts;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Launcher
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (e.Args.Contains("-delcaches"))
+                LaunchHelper.DelCaches();
+            else
+                LaunchHelper.Launch();
+
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Launcher/Scripts/LaunchHelper.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Launcher.Scripts
+{
+    class LaunchHelper
+    {
+        const string FolderName = "browser";
+
+        const string ExeName = "CefFlashBrowser.exe";
+
+        const string ProcName = "CefFlashBrowser";
+
+        static readonly string CurrentPath = AppDomain.CurrentDomain.BaseDirectory;
+
+        static readonly string TargetPath = Path.Combine(CurrentPath, FolderName);
+
+        static readonly string TargetExe = Path.Combine(TargetPath, ExeName);
+
+        public static void KillProcs()
+        {
+            foreach (var proc in Process.GetProcessesByName(ProcName))
+                proc.Kill();
+        }
+
+        public static void DelCaches()
+        {
+            KillProcs();
+
+            //delcaches...
+
+            Launch();
+        }
+
+        public static void Launch()
+        {
+            var info = new ProcessStartInfo()
+            {
+                FileName = TargetExe,
+                WorkingDirectory = FolderName
+            };
+            try
+            {
+                Process.Start(info);
+            }
+            catch
+            {
+                System.Windows.MessageBox.Show("ERROR");
+            }
+        }
+    }
+}
