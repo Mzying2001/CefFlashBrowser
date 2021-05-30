3e5c8c9b9fd3694ec5be20a91d278ea5f095a642
Mzying2001 <Mzying2001@qq.com>
2021-05-30 09:38:40 +0800

    Add Favorites to static data

---
+++ CefFlashBrowser/Models/StaticData/Favorites.cs
@@ -0,0 +1,22 @@
+﻿using CefFlashBrowser.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefFlashBrowser.Models.StaticData
+{
+    static class Favorites
+    {
+        public static ObservableCollection<Website> Items { get; private set; }
+
+        public static void InitFavorites()
+        {
+            Items = new ObservableCollection<Website>();
+            foreach (var website in new FavoritesDataService().GetFavorites())
+                Items.Add(website);
+        }
+    }
+}
