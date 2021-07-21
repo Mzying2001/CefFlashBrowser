f5a21ca92c7108db1c0e2a7099111ebc98236baa
Mzying2001 <Mzying2001@qq.com>
2021-07-21 12:46:51 +0800

    Add ChromiumSwfBrowser

---
+++ CefFlashBrowser/Models/FlashBrowser/ChromiumSwfBrowser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CefFlashBrowser.Models.FlashBrowser
+{
+    public class ChromiumSwfBrowser : FlashBrowserBase
+    {
+
+
+        public string FileName
+        {
+            get { return (string)GetValue(FileNameProperty); }
+            set
+            {
+                var url = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                    $"html/FlashPlayer.html?src={value}");
+                SetValue(FileNameProperty, url);
+            }
+        }
+
+        // Using a DependencyProperty as the backing store for FileName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty FileNameProperty =
+            DependencyProperty.Register("FileName", typeof(string), typeof(ChromiumSwfBrowser), new PropertyMetadata(string.Empty));
+
+
+    }
+}
