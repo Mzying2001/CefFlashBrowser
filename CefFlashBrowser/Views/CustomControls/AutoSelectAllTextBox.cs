5a7a1d5cd64e851314e65428247028ef6d5c2ecd
Mzying2001 <Mzying2001@qq.com>
2021-07-26 10:40:10 +0800

    Add AutoSelectAllTextBox

---
+++ CefFlashBrowser/Views/CustomControls/AutoSelectAllTextBox.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CefFlashBrowser.Views.CustomControls
+{
+    public class AutoSelectAllTextBox : TextBox
+    {
+        public override void BeginInit()
+        {
+            base.BeginInit();
+            PreviewMouseDown += AutoSelectAllTextBox_PreviewMouseDown;
+        }
+
+        private void AutoSelectAllTextBox_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Focus();
+            e.Handled = true;
+        }
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            SelectAll();
+            PreviewMouseDown -= AutoSelectAllTextBox_PreviewMouseDown;
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            PreviewMouseDown += AutoSelectAllTextBox_PreviewMouseDown;
+        }
+    }
+}
