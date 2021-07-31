5dd042fa7ff3df9223cbf14587a6ed38d27470c4
Mzying2001 <Mzying2001@qq.com>
2021-07-31 14:02:54 +0800

    Add loading label

---
+++ CefFlashBrowser/Views/Converters/BoolToVisibility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CefFlashBrowser.Views.Converters
+{
+    public class BoolToVisibility : ValueConverterBase<bool, Visibility>
+    {
+        public override Visibility Convert(bool value, object parameter, CultureInfo culture)
+        {
+            return value ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public override bool ConvertBack(Visibility value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
