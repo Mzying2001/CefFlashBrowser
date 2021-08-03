2c7b9a4d0e96a66e2b4c22bc815dde7dcad3a78c
Mzying2001 <Mzying2001@qq.com>
2021-08-03 18:09:31 +0800

    Add DropableWindow

---
+++ CefFlashBrowser/Views/CustomControls/DropableWindow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CefFlashBrowser.Views.CustomControls
+{
+    public class DropableWindow : Window
+    {
+
+
+        public ICommand OnDropCommand
+        {
+            get { return (ICommand)GetValue(OnDropCommandProperty); }
+            set { SetValue(OnDropCommandProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for OnDropCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OnDropCommandProperty =
+            DependencyProperty.Register("OnDropCommand", typeof(ICommand), typeof(DropableWindow), new PropertyMetadata(null));
+
+
+        protected override void OnDrop(DragEventArgs e)
+        {
+            base.OnDrop(e);
+            if (OnDropCommand != null)
+                OnDropCommand.Execute(e);
+        }
+    }
+}
