675f9a5afa7f89e42d195fa8dee9ad60c8b34e60
Mzying2001 <Mzying2001@qq.com>
2021-10-30 12:51:40 +0800

    Update Converters

--- CefFlashBrowser/Models/Converters/BoolToObject.cs
+++ CefFlashBrowser/Models/Converters/BoolToObject.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CefFlashBrowser.Views.Converters
+namespace CefFlashBrowser.Models.Converters
 {
     public class BoolToObject : ValueConverterBase<bool, object>
     {
--- CefFlashBrowser/Models/Converters/ValueConverterBase.cs
+++ CefFlashBrowser/Models/Converters/ValueConverterBase.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
-namespace CefFlashBrowser.Views.Converters
+namespace CefFlashBrowser.Models.Converters
 {
     public abstract class ValueConverterBase<SourceType, TargetType> : IValueConverter
     {
