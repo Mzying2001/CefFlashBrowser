a63f0bf54e7aa649842f44f3ec03b6d5fbf56b70
Mzying2001 <Mzying2001@qq.com>
2021-10-30 11:20:43 +0800

    Move svgs to Assets

---
+++ CefFlashBrowser/Views/Converters/BoolToObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CefFlashBrowser.Views.Converters
+{
+    public class BoolToObject : ValueConverterBase<bool, object>
+    {
+        public object TrueObject { get; set; }
+        public object FalseObject { get; set; }
+
+        public override object Convert(bool value, object parameter, CultureInfo culture)
+        {
+            return value ? TrueObject : FalseObject;
+        }
+
+        public override bool ConvertBack(object value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
