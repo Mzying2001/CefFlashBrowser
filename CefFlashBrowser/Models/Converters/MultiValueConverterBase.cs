c6654d0c1c0f6ed200bd4b79d4b9b1b43c43bc56
Mzying2001 <Mzying2001@qq.com>
2021-11-06 10:45:18 +0800

    Add MultiValueConverter

---
+++ CefFlashBrowser/Models/Converters/MultiValueConverterBase.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace CefFlashBrowser.Models.Converters
+{
+    public abstract class MultiValueConverterBase<TIn, TOut> : IMultiValueConverter
+    {
+        object IMultiValueConverter.Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert((from i in values select (TIn)i).ToArray(), parameter, culture);
+        }
+
+        object[] IMultiValueConverter.ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            return (from i in ConvertBack((TOut)value, parameter, culture) select (object)i).ToArray();
+        }
+
+        public abstract TOut Convert(TIn[] values, object parameter, CultureInfo culture);
+        public abstract TIn[] ConvertBack(TOut value, object parameter, CultureInfo culture);
+    }
+}
---
+++ CefFlashBrowser/Models/Converters/StringArrNotAllEmpty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace CefFlashBrowser.Models.Converters
+{
+    public class StringArrNotAllEmpty : MultiValueConverterBase<string, bool>
+    {
+        public override bool Convert(string[] values, object parameter, CultureInfo culture)
+        {
+            return values.All(i => !string.IsNullOrWhiteSpace(i));
+        }
+
+        public override string[] ConvertBack(bool value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
