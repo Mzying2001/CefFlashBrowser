4ff8da27fc63897b8f648b7a85fa66d9a977d5fd
Mzying2001 <Mzying2001@qq.com>
2021-11-06 15:30:37 +0800

    Add WindowSizeInfo

---
+++ CefFlashBrowser/Models/WindowSizeInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace CefFlashBrowser.Models
+{
+    public class WindowSizeInfo
+    {
+        public double Left { get; set; }
+        public double Top { get; set; }
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public static WindowSizeInfo GetSizeInfo(Window window)
+        {
+            return new WindowSizeInfo
+            {
+                Left = window.Left,
+                Top = window.Top,
+                Width = window.Width,
+                Height = window.Height
+            };
+        }
+
+        public void Apply(Window window)
+        {
+            window.Left = Left;
+            window.Top = Top;
+            window.Width = Width;
+            window.Height = Height;
+        }
+    }
+}
