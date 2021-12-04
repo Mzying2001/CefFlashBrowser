b0265aaee559ec2df1d0031e8691310f8854feed
Mzying2001 <Mzying2001@qq.com>
2021-12-04 19:20:12 +0800

    Auto decode url

---
+++ CefFlashBrowser/Models/Converters/UrlDecoder.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Net;
+
+namespace CefFlashBrowser.Models.Converters
+{
+    public class UrlDecoder : ValueConverterBase<string, string>
+    {
+        public override string Convert(string value, object parameter, CultureInfo culture)
+        {
+            return WebUtility.UrlDecode(value);
+        }
+
+        public override string ConvertBack(string value, object parameter, CultureInfo culture)
+        {
+            return WebUtility.UrlEncode(value);
+        }
+    }
+}
