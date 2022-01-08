b049299500af78578637696a7da5ac7df6c221c2
Mzying2001 <Mzying2001@qq.com>
2022-01-08 17:14:14 +0800

    Add DefaultWpfMenuHandler

--- CefFlashBrowser/Views/ViewSourceWindow.xaml.cs
+++ CefFlashBrowser/Views/ViewSourceWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CefFlashBrowser.FlashBrowser.Handlers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,6 +37,7 @@ namespace CefFlashBrowser.Views
         public ViewSourceWindow()
         {
             InitializeComponent();
+            browser.MenuHandler = new DefaultWpfMenuHandler();
         }
 
         public ViewSourceWindow(string address) : this()
