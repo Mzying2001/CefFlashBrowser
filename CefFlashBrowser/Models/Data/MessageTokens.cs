a547bfc68609f0a02ceea8899c3d4dfe19ebe713
Mzying2001 <Mzying2001@qq.com>
2022-01-13 23:53:41 +0800

    Remove unnecessary message tokens

--- CefFlashBrowser/Models/Data/MessageTokens.cs
+++ CefFlashBrowser/Models/Data/MessageTokens.cs
@@ -3,9 +3,6 @@
     public static class MessageTokens
     {
         public const string EXIT_BROWSER = "EXIT_BROWSER";
-        public const string EXIT_JSALERT = "EXIT_JSALERT";
-        public const string EXIT_JSCONFIRM = "EXIT_JSCONFIRM";
-        public const string EXIT_JSPROMPT = "EXIT_JSPROMPT";
         public const string EXIT_ADDFAVORITES = "EXIT_ADDFAVORITES";
         public const string EXIT_SELECTLANGUAGE = "EXIT_SELECTLANGUAGE";
         public const string LANGUAGE_CHANGED = "LANGUAGE_CHANGED";
