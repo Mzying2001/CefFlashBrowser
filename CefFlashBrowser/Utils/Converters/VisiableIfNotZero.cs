4e7f508c1a88a56e56cc2b3177b1f0cd8bcaac30
Mzying2001 <Mzying2001@qq.com>
2022-04-03 11:44:26 +0800

    Hide Favorites menu when there are no favorites

---
+++ CefFlashBrowser/Utils/Converters/VisiableIfNotZero.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace CefFlashBrowser.Utils.Converters
+{
+    public class VisiableIfNotZero : ValueConverterBase<int, Visibility>
+    {
+        public override Visibility Convert(int value, object parameter, CultureInfo culture)
+        {
+            return value == 0 ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public override int ConvertBack(Visibility value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
