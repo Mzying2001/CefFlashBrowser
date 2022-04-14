325540d0440baf4f66967bc1a764c4e6215fee89
Mzying2001 <Mzying2001@qq.com>
2022-04-14 12:59:31 +0800

    Fake flash version

--- CefFlashBrowser/FlashBrowser/FlashBrowserBase.cs
+++ CefFlashBrowser/FlashBrowser/FlashBrowserBase.cs
@@ -60,7 +60,7 @@ namespace CefFlashBrowser.FlashBrowser
 #endif
 
             settings.CefCommandLineArgs["enable-system-flash"] = "1";
-            settings.CefCommandLineArgs.Add("ppapi-flash-version", FileVersionInfo.GetVersionInfo(FlashPath).FileVersion.Replace(',', '.'));
+            settings.CefCommandLineArgs.Add("ppapi-flash-version", "99.0.0.999");
             settings.CefCommandLineArgs.Add("ppapi-flash-path", FlashPath);
             Cef.Initialize(settings);
         }
