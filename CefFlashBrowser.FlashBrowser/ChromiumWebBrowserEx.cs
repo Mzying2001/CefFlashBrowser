68b6c8c369721aade8e14c52757ae9dc3ecd699d
Mzying2001 <Mzying2001@qq.com>
2022-05-28 15:23:10 +0800

    Add NotifyWpfFocusHandler

--- CefFlashBrowser.FlashBrowser/ChromiumWebBrowserEx.cs
+++ CefFlashBrowser.FlashBrowser/ChromiumWebBrowserEx.cs
@@ -1,15 +1,38 @@
 ﻿using CefFlashBrowser.WinformCefSharp4WPF;
+using CefSharp;
 using SimpleMvvm.Command;
+using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace CefFlashBrowser.FlashBrowser
 {
     public class ChromiumWebBrowserEx : ChromiumWebBrowser
     {
+        private class NotifyWpfFocusHandler : Handlers.FocusHandler
+        {
+            public override void OnGotFocus(IWebBrowser chromiumWebBrowser, IBrowser browser)
+            {
+                ((IWpfWebBrowser)chromiumWebBrowser).Dispatcher.Invoke(delegate
+                {
+                    DependencyObject d = (DependencyObject)chromiumWebBrowser;
+                    while (VisualTreeHelper.GetParent(d) is DependencyObject parent)
+                    {
+                        d = parent;
+                    }
+                    if (!ReferenceEquals(d, chromiumWebBrowser))
+                    {
+                        FocusManager.SetFocusedElement(d, (IInputElement)chromiumWebBrowser);
+                    }
+                });
+            }
+        }
+
         public ICommand LoadUrlCommand { get; }
 
         public ChromiumWebBrowserEx()
         {
+            FocusHandler = new NotifyWpfFocusHandler();
             LoadUrlCommand = new DelegateCommand<string>(Load);
         }
     }
