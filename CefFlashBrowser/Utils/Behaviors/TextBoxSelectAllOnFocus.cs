b9276971f3363c635eb61c8e907b238cc4b0903f
Mzying2001 <Mzying2001@qq.com>
2022-06-15 13:00:52 +0800

    Add Behaviors

---
+++ CefFlashBrowser/Utils/Behaviors/TextBoxSelectAllOnFocus.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace CefFlashBrowser.Utils.Behaviors
+{
+    public class TextBoxSelectAllOnFocus : Behavior<TextBox>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.PreviewMouseDown += TextBoxPreviewMouseDown;
+            AssociatedObject.GotFocus += TextBoxGotFocus;
+            AssociatedObject.LostFocus += TextBoxLostFocus;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.PreviewMouseDown -= TextBoxPreviewMouseDown;
+            AssociatedObject.GotFocus -= TextBoxGotFocus;
+            AssociatedObject.LostFocus -= TextBoxLostFocus;
+        }
+
+        private void TextBoxPreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            AssociatedObject.Focus();
+        }
+
+        private void TextBoxGotFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AssociatedObject.SelectAll();
+            AssociatedObject.PreviewMouseDown -= TextBoxPreviewMouseDown;
+        }
+
+        private void TextBoxLostFocus(object sender, System.Windows.RoutedEventArgs e)
+        {
+            AssociatedObject.PreviewMouseDown += TextBoxPreviewMouseDown;
+        }
+    }
+}
