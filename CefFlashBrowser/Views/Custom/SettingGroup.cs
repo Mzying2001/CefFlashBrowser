a33b8ea41531d2f2b17dfba81900452f74dda656
Mzying2001 <Mzying2001@qq.com>
2022-09-03 17:14:11 +0800

    Add SettingGroup

---
+++ CefFlashBrowser/Views/Custom/SettingGroup.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace CefFlashBrowser.Views.Custom
+{
+    public class SettingGroup : ItemsControl
+    {
+
+
+        public string Header
+        {
+            get { return (string)GetValue(HeaderProperty); }
+            set { SetValue(HeaderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Header.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty HeaderProperty =
+            DependencyProperty.Register("Header", typeof(string), typeof(SettingGroup), new PropertyMetadata(string.Empty));
+
+
+        static SettingGroup()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(SettingGroup), new FrameworkPropertyMetadata(typeof(SettingGroup)));
+        }
+    }
+}
