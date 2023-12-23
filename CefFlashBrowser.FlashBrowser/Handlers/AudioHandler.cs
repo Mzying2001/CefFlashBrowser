07d81b932fcb934c4b116b30719f1327fc56b6b8
Mzying2001 <Mzying2001@qq.com>
2023-12-23 11:14:02 +0800

    Add AudioHandler

---
+++ CefFlashBrowser.FlashBrowser/Handlers/AudioHandler.cs
@@ -0,0 +1,30 @@
+﻿using CefSharp;
+using CefSharp.Structs;
+using System;
+
+namespace CefFlashBrowser.FlashBrowser.Handlers
+{
+    public class AudioHandler : IAudioHandler
+    {
+        public virtual bool GetAudioParameters(IWebBrowser chromiumWebBrowser, IBrowser browser, ref AudioParameters parameters)
+        {
+            return true;
+        }
+
+        public virtual void OnAudioStreamError(IWebBrowser chromiumWebBrowser, IBrowser browser, string errorMessage)
+        {
+        }
+
+        public virtual void OnAudioStreamPacket(IWebBrowser chromiumWebBrowser, IBrowser browser, IntPtr data, int noOfFrames, long pts)
+        {
+        }
+
+        public virtual void OnAudioStreamStarted(IWebBrowser chromiumWebBrowser, IBrowser browser, AudioParameters parameters, int channels)
+        {
+        }
+
+        public virtual void OnAudioStreamStopped(IWebBrowser chromiumWebBrowser, IBrowser browser)
+        {
+        }
+    }
+}
