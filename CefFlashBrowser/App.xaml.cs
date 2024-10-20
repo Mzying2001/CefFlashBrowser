030b87bf5de1667d47f85959dc7e22213091c949
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2024-10-20 18:08:46 +0800

    Add Program.cs

--- CefFlashBrowser/App.xaml.cs
+++ CefFlashBrowser/App.xaml.cs
@@ -1,11 +1,7 @@
-﻿using CefFlashBrowser.FlashBrowser;
-using CefFlashBrowser.Models.Data;
+﻿using CefFlashBrowser.Models.Data;
 using CefFlashBrowser.Utils;
-using CefSharp;
 using System;
 using System.Diagnostics;
-using System.IO;
-using System.Reflection;
 using System.Windows;
 
 namespace CefFlashBrowser
@@ -17,27 +13,12 @@ namespace CefFlashBrowser
     {
         private static bool _restart = false;
 
-        public App() : base()
-        {
-            Win32.SetDllDirectory(GlobalData.CefDllPath);
-
-            AppDomain.CurrentDomain.AssemblyResolve += (s, e) =>
-            {
-                // Load CefSharp dlls
-                string assemblyName = new AssemblyName(e.Name).Name;
-                string assemblyPath = Path.Combine(GlobalData.CefDllPath, assemblyName + ".dll");
-                return File.Exists(assemblyPath) ? Assembly.LoadFrom(assemblyPath) : null;
-            };
-        }
-
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             try
             {
-                InitCefFlash();
-
                 if (GlobalData.Settings.FirstStart)
                 {
                     if (WindowManager.ShowSelectLanguageDialog())
@@ -86,50 +67,5 @@ namespace CefFlashBrowser
             _restart = true;
             Current.Shutdown();
         }
-
-
-        /*====================================================================================================*/
-
-
-        private static void InitCefFlash()
-        {
-            Environment.SetEnvironmentVariable("ComSpec", GlobalData.EmptyExePath); //Remove black popup window
-
-            var settings = new CefFlashSettings()
-            {
-                Locale = LanguageManager.GetLocale(GlobalData.Settings.Language),
-                CachePath = GlobalData.CachesPath,
-                PpapiFlashPath = GlobalData.FlashPath,
-                EnableSystemFlash = true
-            };
-
-            if (GlobalData.Settings.FakeFlashVersionSetting.Enable)
-            {
-                settings.PpapiFlashVersion = GlobalData.Settings.FakeFlashVersionSetting.FlashVersion;
-            }
-            else
-            {
-                settings.PpapiFlashVersion = FileVersionInfo.GetVersionInfo(GlobalData.FlashPath).FileVersion.Replace(',', '.');
-            }
-
-            if (GlobalData.Settings.UserAgentSetting.EnableCustom)
-            {
-                settings.UserAgent = GlobalData.Settings.UserAgentSetting.UserAgent;
-            }
-
-            if (GlobalData.Settings.ProxySettings.EnableProxy)
-            {
-                var proxySettings = GlobalData.Settings.ProxySettings;
-                CefSharpSettings.Proxy = new ProxyOptions(proxySettings.IP, proxySettings.Port, proxySettings.UserName, proxySettings.Password);
-            }
-
-#if !DEBUG
-            settings.LogSeverity = LogSeverity.Disable;
-#endif
-
-            settings.CefCommandLineArgs["autoplay-policy"] = "no-user-gesture-required";
-            Cef.Initialize(settings);
-        }
-
     }
 }
