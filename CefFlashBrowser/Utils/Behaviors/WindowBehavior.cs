a3465e2e099ab71d64344fdd42e8e1f4f577f54a
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-02-17 22:26:03 +0800

    Add `WindowBehavior`

---
+++ CefFlashBrowser/Utils/Behaviors/WindowBehavior.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xaml.Behaviors;
+using SimpleMvvm.Command;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CefFlashBrowser.Utils.Behaviors
+{
+    public class WindowBehavior : Behavior<Window>
+    {
+        private ICommand _closeWindowCommand;
+        public ICommand CloseWindowCommand => _closeWindowCommand;
+
+        public WindowBehavior()
+        {
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            _closeWindowCommand = new DelegateCommand(AssociatedObject.Close);
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            _closeWindowCommand = null;
+        }
+    }
+}
