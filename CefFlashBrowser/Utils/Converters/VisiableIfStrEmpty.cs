353fddddf68be9bbb09b8f3ea8a3755170ee5dd1
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-03-02 10:49:59 +0800

    Add svg page icon

---
+++ CefFlashBrowser/Utils/Converters/VisiableIfStrEmpty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+
+namespace CefFlashBrowser.Utils.Converters
+{
+    class VisiableIfStrEmpty : ValueConverterBase<string, Visibility>
+    {
+        public override Visibility Convert(string value, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrWhiteSpace(value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public override string ConvertBack(Visibility value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
