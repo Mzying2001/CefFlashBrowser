7bbada1bffe6a1f156125d2e0c99792a44e8ed43
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2025-04-13 16:08:26 +0800

    Add converter `ObjectToString`

---
+++ CefFlashBrowser/Utils/Converters/ObjectToString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CefFlashBrowser.Utils.Converters
+{
+    public class ObjectToString : ValueConverterBase<object, string>
+    {
+        public override string Convert(object value, object parameter, CultureInfo culture)
+        {
+            return value?.ToString();
+        }
+
+        public override object ConvertBack(string value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
