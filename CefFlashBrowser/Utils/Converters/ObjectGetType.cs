51f17af0c628d891adcc6b70789d35340492bf50
Mzying2001 <41951400+Mzying2001@users.noreply.github.com>
2026-01-18 11:30:23 +0800

    Refactor favorites menu to use CompositeCollection and type-based triggers
    Reworked the favorites menu in MainWindow.xaml to use a CompositeCollection for combining static and dynamic items, and applied a DataTrigger with the new ObjectGetType converter to handle Website items. Added ObjectGetType converter and updated Converters.xaml to register it. This improves maintainability and flexibility of the favorites menu rendering and command binding.

---
+++ CefFlashBrowser/Utils/Converters/ObjectGetType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace CefFlashBrowser.Utils.Converters
+{
+    public class ObjectGetType : ValueConverterBase<object, Type>
+    {
+        public override Type Convert(object value, object parameter, CultureInfo culture)
+        {
+            return value?.GetType();
+        }
+
+        public override object ConvertBack(Type value, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
