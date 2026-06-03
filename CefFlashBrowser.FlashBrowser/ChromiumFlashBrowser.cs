7bf8571eb7706d39beaa10c493cdb518573bb497
Lortzing <zhshkang0@outlook.com>
2026-06-03 20:30:12 +0800

    Add page-local input memory support

--- CefFlashBrowser.FlashBrowser/ChromiumFlashBrowser.cs
+++ CefFlashBrowser.FlashBrowser/ChromiumFlashBrowser.cs
@@ -43,6 +43,103 @@ namespace CefFlashBrowser.FlashBrowser
     });
 })();";
 
+        private const string InputMemoryBootstrapScript = @"
+(function() {
+    if (window.__cefInputMemory) return;
+    var memory = {
+        recording: false,
+        events: [],
+        startTime: 0,
+        startedAt: null,
+        start: function() {
+            this.events = [];
+            this.startTime = Date.now();
+            this.startedAt = document.activeElement;
+            this.recording = true;
+        },
+        stop: function() {
+            this.recording = false;
+        },
+        clear: function() {
+            this.events = [];
+            this.recording = false;
+        },
+        state: function() {
+            return { recording: this.recording, count: this.events.length };
+        },
+        record: function(e) {
+            if (!this.recording || !e || !e.type) return;
+            this.events.push({
+                type: e.type,
+                time: Date.now() - this.startTime,
+                x: typeof e.clientX === 'number' ? e.clientX : null,
+                y: typeof e.clientY === 'number' ? e.clientY : null,
+                button: typeof e.button === 'number' ? e.button : 0,
+                buttons: typeof e.buttons === 'number' ? e.buttons : 0,
+                deltaX: typeof e.deltaX === 'number' ? e.deltaX : 0,
+                deltaY: typeof e.deltaY === 'number' ? e.deltaY : 0,
+                key: e.key || '',
+                code: e.code || '',
+                keyCode: e.keyCode || 0,
+                ctrlKey: !!e.ctrlKey,
+                shiftKey: !!e.shiftKey,
+                altKey: !!e.altKey,
+                metaKey: !!e.metaKey
+            });
+        },
+        targetFor: function(item) {
+            if (item.x !== null && item.y !== null) {
+                return document.elementFromPoint(item.x, item.y) || document.body || document.documentElement;
+            }
+            return this.startedAt || document.activeElement || document.body || document.documentElement;
+        },
+        dispatch: function(item) {
+            var target = this.targetFor(item);
+            var init = {
+                bubbles: true,
+                cancelable: true,
+                composed: true,
+                clientX: item.x || 0,
+                clientY: item.y || 0,
+                button: item.button || 0,
+                buttons: item.buttons || 0,
+                deltaX: item.deltaX || 0,
+                deltaY: item.deltaY || 0,
+                key: item.key || '',
+                code: item.code || '',
+                keyCode: item.keyCode || 0,
+                which: item.keyCode || 0,
+                ctrlKey: !!item.ctrlKey,
+                shiftKey: !!item.shiftKey,
+                altKey: !!item.altKey,
+                metaKey: !!item.metaKey
+            };
+            var evt;
+            if (item.type.indexOf('key') === 0) {
+                evt = new KeyboardEvent(item.type, init);
+            } else if (item.type === 'wheel') {
+                evt = new WheelEvent(item.type, init);
+            } else {
+                evt = new MouseEvent(item.type, init);
+            }
+            target.dispatchEvent(evt);
+        },
+        play: function() {
+            var list = this.events.slice(0);
+            if (!list.length) return;
+            this.recording = false;
+            list.forEach(function(item) {
+                window.setTimeout(function() { memory.dispatch(item); }, Math.max(0, item.time || 0));
+            });
+        }
+    };
+
+    ['mousedown','mouseup','mousemove','click','dblclick','wheel','keydown','keyup'].forEach(function(type) {
+        document.addEventListener(type, function(e) { memory.record(e); }, true);
+    });
+    window.__cefInputMemory = memory;
+})();";
+
         static ChromiumFlashBrowser()
         {
             BlockedSwfsProperty = DependencyProperty.Register(
@@ -68,11 +165,49 @@ namespace CefFlashBrowser.FlashBrowser
             get => (bool)GetValue(HasBlockedSwfsProperty);
         }
 
+        public bool IsInputMemoryRecording { get; private set; }
+
         public void EnableFlashAcceleration()
         {
             if (CanExecuteJavascriptInMainFrame)
             {
                 ExecuteScriptAsync(FlashAccelerationScript);
+            }
+        }
+
+        public void StartInputMemoryRecording()
+        {
+            IsInputMemoryRecording = true;
+            if (CanExecuteJavascriptInMainFrame)
+            {
+                ExecuteScriptAsync(InputMemoryBootstrapScript + "\nwindow.__cefInputMemory.start();");
+            }
+        }
+
+        public void StopInputMemoryRecording()
+        {
+            IsInputMemoryRecording = false;
+            if (CanExecuteJavascriptInMainFrame)
+            {
+                ExecuteScriptAsync(InputMemoryBootstrapScript + "\nwindow.__cefInputMemory.stop();");
+            }
+        }
+
+        public void ReplayInputMemory()
+        {
+            IsInputMemoryRecording = false;
+            if (CanExecuteJavascriptInMainFrame)
+            {
+                ExecuteScriptAsync(InputMemoryBootstrapScript + "\nwindow.__cefInputMemory.play();");
+            }
+        }
+
+        public void ClearInputMemory()
+        {
+            IsInputMemoryRecording = false;
+            if (CanExecuteJavascriptInMainFrame)
+            {
+                ExecuteScriptAsync(InputMemoryBootstrapScript + "\nwindow.__cefInputMemory.clear();");
             }
         }
 
@@ -83,6 +218,7 @@ namespace CefFlashBrowser.FlashBrowser
 
             if (e.Frame.IsMain)
             {
+                IsInputMemoryRecording = false;
                 BlockedSwfs.Clear();
                 SetCurrentValue(HasBlockedSwfsProperty, false);
             }
@@ -95,6 +231,7 @@ namespace CefFlashBrowser.FlashBrowser
             if (e.Frame.IsMain)
             {
                 e.Frame.ExecuteJavaScriptAsync(FlashAccelerationScript);
+                e.Frame.ExecuteJavaScriptAsync(InputMemoryBootstrapScript);
             }
         }
 
